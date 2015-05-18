651ff9411f18ee01de956d911bf9388c7d8ad680
Pedro Plowman <pedrofp@me.com>
2015-05-19 04:10:35 +1000

    licence statement

--- FlotChartsAsset.php
+++ FlotChartsAsset.php
@@ -5,7 +5,7 @@
  * @copyright Copyright &copy; Pedro Plowman, https://github.com/p2made, 2015
  * @author Pedro Plowman
  * @package p2made/yii2-asset-collection
- *
+ * @license MIT
  */
 
 namespace p2made\assets;
