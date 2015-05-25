4348718625fce82b12d1f4922473733e3b56620d
Pedro Plowman <pedrofp@me.com>
2015-05-25 17:59:13 +1000

    v3.0.0 Major Refactor
    Moved resource data to asset classes & simplified init methods.

---
+++ DataTablesAsset.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DataTablesAsset.php
+ *
+ * @copyright Copyright &copy; Pedro Plowman, https://github.com/p2made, 2015
+ * @author Pedro Plowman
+ * @package p2made/yii2-asset-collection
+ * @license MIT
+ */
+
+namespace p2made\assets;
+
+class DataTablesAsset extends P2AssetBundle
+{
+	private $resourceData = array(
+		'sourcePath' => '@vendor/p2made/yii2-asset-collection/assets/DataTables-1.10.7',
+		'pub-css' => [
+			'plugins/integration/bootstrap/3/dataTables.bootstrap.css',
+			'extensions/Responsive/css/dataTables.responsive.css',
+		],
+		'cdn-css' => [
+			'//cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css',
+			'//cdn.datatables.net/responsive/1.0.3/css/dataTables.responsive.css',
+		],
+		'pub-js'  => [
+			'media/js/jquery.dataTables.min.js',
+			'plugins/integration/bootstrap/3/dataTables.bootstrap.min.js',
+		],
+		'cdn-js'  => [
+			'//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js',
+			'//cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.js',
+		],
+	);
+
+	public $depends = [
+		'p2made\assets\JqueryAsset',
+	];
+
+	public function init()
+	{
+		$this->configureAsset($this->resourceData);
+		parent::init();
+	}
+}
---
+++ PrettyPhotoAsset.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PrettyPhotoAsset.php
+ *
+ * @copyright Copyright &copy; Pedro Plowman, https://github.com/p2made, 2015
+ * @author Pedro Plowman
+ * @package p2made/yii2-asset-collection
+ * @license MIT
+ */
+
+namespace p2made\assets;
+
+class PrettyPhotoAsset extends P2AssetBundle
+{
+	private $resourceData = array(
+		'sourcePath' => '@vendor/p2made/yii2-asset-collection/assets/prettyPhoto_3.1.6',
+		'pub-css' => [
+			'css/prettyPhoto.min.css',
+		],
+		'cdn-css' => [
+			'//cdn.jsdelivr.net/prettyphoto/3.1.5/css/prettyPhoto.css',
+		],
+		'pub-js'  => [
+			'js/jquery.prettyPhoto.min.js',
+		],
+		'cdn-js'  => [
+			'//cdn.jsdelivr.net/prettyphoto/3.1.5/js/jquery.prettyPhoto.js',
+		],
+	);
+
+	public function init()
+	{
+		$this->configureAsset($this->resourceData);
+		parent::init();
+	}
+}
